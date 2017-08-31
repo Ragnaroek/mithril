608c668221227613e1380ea452f5751bb350803f
Michael Bohn <michael.bohn@freiheit.com>
2017-08-31 06:56:02 +0200

    nearly finished mem-loop

--- src/cryptonight/hash.rs
+++ src/cryptonight/hash.rs
@@ -1,8 +1,9 @@
-
 use super::keccak;
 use super::aes;
 use u64x2::u64x2;
 use std::boxed::Box;
+
+use super::super::byte_string;
 
 pub const MEM_SIZE : usize = 2097152 / 16;
 const ITERATIONS : u32 = 524288;
@@ -11,7 +12,7 @@ pub fn hash(input: &[u8]) {
 
     //scratchpad init
     let state = keccak::keccak(input);
-    let scratchpad = init_scratchpad(&state);
+    let mut scratchpad = init_scratchpad(&state);
 
     let a = u64x2::read(&state[0..16]) ^ u64x2::read(&state[32..48]);
     let b = u64x2::read(&state[16..32]) ^ u64x2::read(&state[48..64]);
@@ -20,8 +21,45 @@ pub fn hash(input: &[u8]) {
     println!("b={:?}", super::super::byte_string::u64x2_to_string(b));
 
     for i in 0..ITERATIONS {
+        let mut ix = scratchpad_addr(&a);
+        if i == 0 {
+            println!("ix={:x}", ix);
+            println!("scratchpad[ix]={:}", byte_string::u64x2_to_string(scratchpad[ix]));
+        }
+        let aes_result = aes::aes_round(scratchpad[ix], a);
+        scratchpad[ix] = b ^ aes_result;
 
+        if i == 0 {
+            println!("b xor aes={:}", byte_string::u64x2_to_string(scratchpad[ix]));
+            println!("aes_result={:}", byte_string::u64x2_to_string(aes_result));
+        }
+
+        ix = scratchpad_addr(&aes_result);
+        let add_r = ebyte_add(&a, &ebyte_mul(&aes_result, &scratchpad[ix]));
+        scratchpad[ix] = add_r;
+        if i == 0 {
+            println!("ix={:x}", ix);
+            println!("add_r: {:?}", byte_string::u64x2_to_string(add_r));
+        }
+
+        // a = TODO write xor add and mul arg
+        b = aes_result;
     }
+}
+
+pub fn ebyte_mul(a: &u64x2, b: &u64x2) -> u64x2 {
+    let r0 = u128::from(a.0);
+    let r1 = u128::from(b.0);
+    let r = r0 * r1;
+    return u64x2((r >> 64) as u64, r as u64);
+}
+
+pub fn ebyte_add(a: &u64x2, b: &u64x2) -> u64x2 {
+    return u64x2(a.0.wrapping_add(b.0), a.1.wrapping_add(b.1));
+}
+
+pub fn scratchpad_addr(u: &u64x2) -> usize {
+    return ((u.0 & 0x1FFFF0) >> 4) as usize;
 }
 
 pub fn init_scratchpad(state: &[u8; 200]) -> Box<[u64x2; MEM_SIZE]>{
@@ -49,7 +87,6 @@ pub fn init_scratchpad(state: &[u8; 200]) -> Box<[u64x2; MEM_SIZE]>{
 
     return scratchpad;
 }
-
 
 pub fn aes_round_keys(state: &[u8; 200]) -> [u64x2;10] {
     let mut r : [u64x2;10] = [u64x2(0,0);10];
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use mithril::byte_string;
 use mithril::cryptonight::hash;
 
 fn main() {
-    let input = byte_string::string_to_u8_array("0505f8fcc9cc05b7844da6f865bb3f5db086fe87af9f0ddc21a20a06a9b6c76ce510bb795ed20425004000f8ad73db1c595d3a6e2c06368ee411c184d7be7b15a5e4da928c19a958303d7a01");
+    let input = byte_string::string_to_u8_array("0505ead6b8cd05609fe563801f8d2689108eef9fb731308d2c718f5c4fceb3d9cb4558216b7bac2d004000a30b91824b2f964a613bddfefa99d000116b4978953d354a4a831f59e8fe124903");
 
     hash::hash(&input);
 }
