290d72cd4341d7711a5ebdcdcffdaf91de55fcb7
Michael Bohn <michael.bohn@freiheit.com>
2017-12-05 05:42:44 +0100

    fix non-compiling moved tests

--- tests/aes.rs
+++ tests/aes.rs
@@ -1,12 +1,15 @@
 extern crate mithril;
 
 use mithril::cryptonight::aes;
+use mithril::cryptonight::aes::{AESSupport};
+use mithril::cryptonight::keccak;
 use mithril::u64x2::u64x2;
+use mithril::byte_string;
 
 #[test]
 fn test_aes_round_hardware() {
 
-    let aes = aes::new(aes::AESSupport::HW);
+    let aes = aes::new(AESSupport::HW);
 
     let result = aes.aes_round(u64x2(0xd822831cb9e31fcf,0x8818ec8e51cbeb2e),
                                 u64x2(0x0d137d1656ef4daf,0x4653c1691bbe16e8));
@@ -39,4 +42,25 @@ fn test_aes_round_keys_hardware() {
     assert_eq!(byte_string::u64x2_to_string(keys[8]), "a3764ef44da82dfe35fb183250602933");
     assert_eq!(byte_string::u64x2_to_string(keys[9]), "278251bd1171fedc338012fcae284a11");
 }
-//TODO Same test with software AES
+
+/*
+#[test]
+fn test_aes_round_keys_software() {
+    let input = byte_string::string_to_u8_array("05059fa5a8cc05b2df5d8fa271bb2d0304d4b1842f0f50844b735746db97ee5c196c647c3a5adc0c000000640680be903f504e896daebe42cdbe11e1a938d5c7fb2d64baa6356fe6fbacb704");
+    let a = keccak::keccak(&input);
+    let aes = aes::new(AESSupport::SW);
+    let input0 = u64x2::read(&a[0..16]);
+    let input1 = u64x2::read(&a[16..32]);
+    let keys = aes.gen_round_keys(input0, input1);
+
+    assert_eq!(byte_string::u64x2_to_string(keys[0]), "4c73438521575791be9c2c292f259ec4");
+    assert_eq!(byte_string::u64x2_to_string(keys[1]), "2dba7e0233542a04a9e58cf7213e2f56");
+    assert_eq!(byte_string::u64x2_to_string(keys[2]), "8b45520bc736118ee661461f58fd6a36");
+    assert_eq!(byte_string::u64x2_to_string(keys[3]), "ab5bf78c86e1898eb5b5a38a1c502f7d");
+    assert_eq!(byte_string::u64x2_to_string(keys[4]), "968d56c61dc804cddafe15433c9f535c");
+    assert_eq!(byte_string::u64x2_to_string(keys[5]), "14024341bf59b4cd39b83d438c0d9ec9");
+    assert_eq!(byte_string::u64x2_to_string(keys[6]), "eede630a785335cc659b3101bf652442");
+    assert_eq!(byte_string::u64x2_to_string(keys[7]), "36f3af6122f1ec209da858eda41065ae");
+    assert_eq!(byte_string::u64x2_to_string(keys[8]), "a3764ef44da82dfe35fb183250602933");
+    assert_eq!(byte_string::u64x2_to_string(keys[9]), "278251bd1171fedc338012fcae284a11");
+}*/
