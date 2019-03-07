80ba95a6e5d3d9cb4efd82bf842bdb1534f2e80c
Michael Bohn <michael.bohn@freiheit.com>
2019-03-07 06:34:07 +0100

    fix benchmarks, update benchmarks results for new version

--- benches/hash.rs
+++ benches/hash.rs
@@ -14,30 +14,30 @@ use mithril::cryptonight::aes::{AESSupport};
 
 #[bench]
 fn bench_hash_with_hardware(b: &mut Bencher) {
-    let input1 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739430000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
-    let input2 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739420000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
-    let input3 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739410000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
+    let input1 = byte_string::string_to_u8_array("09099aebd3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c60d2f907");
+    let input2 = byte_string::string_to_u8_array("09099aebd3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c66d26666");
+    let input3 = byte_string::string_to_u8_array("66666666d3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c66d26666");
     let mut scratchpad : Box<[u64x2; MEM_SIZE]> = box [u64x2(0,0); MEM_SIZE];
 
     let aes = aes::new(AESSupport::HW);
     b.iter(|| {
-        hash::hash(&mut scratchpad, &input1, &aes, hash::HashVersion::Version7);
-        hash::hash(&mut scratchpad, &input2, &aes, hash::HashVersion::Version7);
-        hash::hash(&mut scratchpad, &input3, &aes, hash::HashVersion::Version7);
+        hash::hash(&mut scratchpad, &input1, &aes);
+        hash::hash(&mut scratchpad, &input2, &aes);
+        hash::hash(&mut scratchpad, &input3, &aes);
     });
 }
 
 #[bench]
 fn bench_hash_with_software(b: &mut Bencher) {
-    let input1 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739430000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
-    let input2 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739420000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
-    let input3 = byte_string::string_to_u8_array("0707cff699d605f7eb4dbdcad3a38b462b52e9b8ecdf06fb4c95bc5b058a177f84d327f27db739410000000363862429fb90c0fc35fcb9f760c484c8532ee5f2a7cbea4e769d44cd12a7f201");
+    let input1 = byte_string::string_to_u8_array("09099aebd3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c60d2f907");
+    let input2 = byte_string::string_to_u8_array("09099aebd3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c66d26666");
+    let input3 = byte_string::string_to_u8_array("66666666d3e1057aad462f2d998d8b9adcf16e03a5bf1820728240eefe433735904fcf663eeb1d00000000b0203ca955ed446e47ab9e884941bc67c75ecb06e444036aafc7ff442c66d26666");
     let mut scratchpad : Box<[u64x2; MEM_SIZE]> = box [u64x2(0,0); MEM_SIZE];
 
     let aes = aes::new(AESSupport::SW);
     b.iter(|| {
-        hash::hash(&mut scratchpad, &input1, &aes, hash::HashVersion::Version7);
-        hash::hash(&mut scratchpad, &input2, &aes, hash::HashVersion::Version7);
-        hash::hash(&mut scratchpad, &input3, &aes, hash::HashVersion::Version7);
+        hash::hash(&mut scratchpad, &input1, &aes);
+        hash::hash(&mut scratchpad, &input2, &aes);
+        hash::hash(&mut scratchpad, &input3, &aes);
     });
 }
